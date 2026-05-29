1911005e8513b997c5bf112de00adc8aa1e4eaaf
hosseincollege <hosseincollege@gmail.com>
2026-05-29 21:17:59 +0330

    Final working

--- src/Section.js
+++ src/Section.js
@@ -3,6 +3,7 @@
 import hiweb from "./lessons/hiweb/index";
 import university from "./lessons/university/index";
 import computer from "./lessons/computer/index";
+import history from "./lessons/history";
 import complex from "./lessons/complex";
 import advancedNet from "./lessons/advancedNet";
 import arshad from "./lessons/arshad";
@@ -15,10 +16,11 @@ const SECTIONS = [
   { key: "hiweb", title: "1. های وب", color: "#d21919", raw: hiweb },
   { key: "university", title: "2. دانشگاه", color: "#feed00", raw: university },
   { key: "computer", title: "3. کامپیوتر", color: "#023ee3", raw: computer },
-  { key: "complex", title: "4. مجتمع", color: "#ff7043", raw: complex },
-  { key: "advancedNet", title: "5. شبکه های پیشرفته", color: "#39ab52", raw: advancedNet },
-  { key: "arshad", title: "6. arshad", color: "#37c943", raw: arshad },
-  { key: "quran", title: "7. اندیشه اسلامی", color: "#6cff43", raw: quranThought },
+  { key: "history", title: "4. تاریخ", color: "#43d3ff", raw: history },
+  { key: "complex", title: "5. مجتمع", color: "#ff7043", raw: complex },
+  { key: "advancedNet", title: "6. شبکه های پیشرفته", color: "#39ab52", raw: advancedNet },
+  { key: "arshad", title: "7. arshad", color: "#37c943", raw: arshad },
+  { key: "quran", title: "8. اندیشه اسلامی", color: "#6cff43", raw: quranThought },
 ];
 
 export default SECTIONS;
