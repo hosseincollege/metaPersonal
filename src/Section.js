0e49e74fa341e0387cb13297d2a176335fe233ee
hosseincollege <hosseincollege@gmail.com>
2026-06-02 21:44:36 +0330

    Final working

--- src/Section.js
+++ src/Section.js
@@ -5,8 +5,6 @@ import university from "./lessons/university/index";
 import computer from "./lessons/computer/index";
 import history from "./lessons/history";
 import complex from "./lessons/complex";
-import advancedNet from "./lessons/advancedNet";
-import arshad from "./lessons/arshad";
 import quranThought from "./lessons/quranThought";
 
 // ======================
@@ -18,9 +16,7 @@ const SECTIONS = [
   { key: "computer", title: "3. کامپیوتر", color: "#023ee3", raw: computer },
   { key: "history", title: "4. تاریخ", color: "#43d3ff", raw: history },
   { key: "complex", title: "5. مجتمع", color: "#ff7043", raw: complex },
-  { key: "advancedNet", title: "6. شبکه های پیشرفته", color: "#39ab52", raw: advancedNet },
-  { key: "arshad", title: "7. arshad", color: "#37c943", raw: arshad },
-  { key: "quran", title: "8. اندیشه اسلامی", color: "#6cff43", raw: quranThought },
+  { key: "quran", title: "6. اندیشه اسلامی", color: "#6cff43", raw: quranThought },
 ];
 
 export default SECTIONS;
