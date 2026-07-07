18f7d080f4412434b7e8efb5c1a1bb457b2640e4
hosseincollege <hosseincollege@gmail.com>
2026-07-07 23:39:53 +0330

    Final working

--- src/lessons/hiweb/index.js
+++ src/lessons/hiweb/index.js
@@ -4,10 +4,16 @@ import section1 from "./1- کلیات";
 import section2 from "./2- رهکام";
 import section3 from "./3- کار های جانبی";
 import section4 from "./4- دانشنامه";
+import section5 from "./5- USO دیتا";
+import section6 from "./6- USO ایرانسل";
+import section7 from "./7- VSAT";
 
 export default [
   section1,
   section2,
   section3,
   section4,
+  section5,
+  section6,
+  section7,
 ];
