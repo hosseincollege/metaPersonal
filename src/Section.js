42d3cb924fc22e9efa9a873b242b24c7141264e9
hosseincollege <hosseincollege@gmail.com>
2026-07-20 07:17:09 +0330

    Final working

--- src/Section.js
+++ src/Section.js
@@ -11,7 +11,7 @@ import quranThought from "./lessons/quranThought";
 // ======================
 const SECTIONS = [
   { key: "hiweb", title: "1. های وب", color: "#d21919", raw: hiweb },
-  { key: "university", title: "2. دانشگاه", color: "#d2c319", raw: university },
+  { key: "university", title: "2. دانشگاه", color: "#ead80d", raw: university },
   { key: "computer", title: "3. کامپیوتر", color: "#1960d2", raw: computer },
   { key: "complex", title: "4. مجتمع", color: "#ff7043", raw: complex },
   { key: "quran", title: "5. طرح کلی اندیشه اسلامی", color: "#19d219", raw: quranThought },
